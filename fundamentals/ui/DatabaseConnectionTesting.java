be60d26f6524d67f97f7af0d14af3cf0cef8f841
Arun <prith000@citymail.cuny.edu>
2018-01-18 13:46:52 -0500

    26. More JDBC, add DatabaseConnectionTesting.java

--- fundamentals/ui/DatabaseConnectionTesting.java
+++ fundamentals/ui/DatabaseConnectionTesting.java
@@ -2,30 +2,56 @@ package core.fundamentals.ui;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DatabaseConnectionTesting {
   public static void main(String[] args) {
+    updateData("UPDATE materials SET title = ? WHERE id = ?", "Second title", 1);
+    retrieveResultSet("SELECT * FROM materials");
+  }
+
+  public static void updateData(String sql, String title, int id) {
     try {
       Class.forName("org.apache.derby.jdbc.ClientDriver");
-      Connection conn = DriverManager.getConnection("jdbc:derby://localhost/library");
-
-      Statement stm = conn.createStatement();
-      ResultSet rs = stm.executeQuery("SELECT * FROM materials");
-
-      // Do something
-      while (rs.next()) {
-        System.out.println(rs.getString("title"));
-      }
-      
-      rs.close();
-      stm.close();
-      conn.close();
-
-    } catch (Exception e) {
-      System.out.println("Something went wrong");
+      try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost/library")) {
+        try (PreparedStatement stm = conn.prepareStatement(sql)) {
+        		stm.setString(1, title);
+        		stm.setInt(2, id);
+          int results = stm.executeUpdate(sql); 
+          System.out.println("Records amended: " + results);
+        }
+      } 
+    } catch (ClassNotFoundException e) {
+      System.out.println("Update failed, Something went wrong");
+      System.out.println(e);
+    } catch (SQLException e) {
+      System.out.println("Update failed, Something went wrong");
       System.out.println(e);
     }
   }
+
+  public static void retrieveResultSet(String sql) {
+    try {
+      Class.forName("org.apache.derby.jdbc.ClientDriver");
+      try (Connection conn = DriverManager.getConnection("jdbc:derby://localhost/library")) {
+        try (Statement stm = conn.createStatement()) {
+          try (ResultSet rs = stm.executeQuery(sql)) {
+            while (rs.next()) {
+              System.out.println(rs.getString("title"));
+            }
+          } 
+        }
+      } 
+    } catch (ClassNotFoundException e) {
+      System.out.println("Retrieve failed, Something went wrong");
+      System.out.println(e);
+    } catch (SQLException e) {
+      System.out.println("Retrieve failed, Something went wrong");
+      System.out.println(e);
+    }
+  }
+
 }
