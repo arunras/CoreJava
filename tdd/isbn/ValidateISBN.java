6251809bff4ab95aa75d856b4e8e7ace6e81f3fb
Arun <prith000@citymail.cuny.edu>
2018-01-19 20:06:50 -0500

    tdd: first tests

---
+++ tdd/isbn/ValidateISBN.java
@@ -0,0 +1,11 @@
+package core.tdd.isbn;
+
+public class ValidateISBN {
+  public boolean checkISBN(String isbn) {
+    int total = 0;
+    for (int i = 0; i < 10; i++) {
+      total += isbn.charAt(i) * (10 - i); 
+    }
+    return (total % 11 == 0) ? true : false;
+  }
+}
