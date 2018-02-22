4ce352cae9212f5fd8d0129b4ddae3284a5fb8bf
Arun <prith000@citymail.cuny.edu>
2018-02-22 04:04:15 -0500

    6. REST Services

---
+++ jee/EmployeeManagementServerApplication/src/core/jee/employeemanagement/rest/EmployeeResource.java
@@ -0,0 +1,33 @@
+package core.jee.employeemanagement.rest;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import core.jee.employeemanagement.EmployeeManagementServiceLocal;
+import core.jee.employeemanagement.domain.Employee;
+
+@Stateless
+@Path("/employees")
+public class EmployeeResource {
+	@Inject
+	private EmployeeManagementServiceLocal service;
+	
+	@GET
+	@Produces("application/XML")
+	public List<Employee> getAllEmployees() {
+		return service.getAllEmployees();
+	}
+	
+	@GET
+	@Produces("application/XML")
+	@Path("{employeeNo}")
+	public Employee findEmployeeById(@PathParam("employeeNo") int id) {
+		return service.getById(id);
+	}
+}
