3bcd556cac2bef3d2d9bc7b977af7395edc71203
Arun <prith000@citymail.cuny.edu>
2018-02-23 11:10:31 -0500

    2. Sending a Message

---
+++ jee/BookStorePOS/src/SendMessage.java
@@ -0,0 +1,56 @@
+import java.util.Date;
+import java.util.Properties;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+
+public class SendMessage {
+
+	public static void main(String[] args) {
+		
+		Properties jndiProperties = new Properties();
+		jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+		jndiProperties.put("jboss.naming.client.ejb.context","true");
+		jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY,"org.jboss.naming.remote.client.InitialContextFactory");
+		jndiProperties.put(Context.PROVIDER_URL,"http-remoting://localhost:8080");
+		
+		Connection connection = null;
+		
+		try {
+			Context ctx = new InitialContext(jndiProperties);
+			Queue queue = (Queue)ctx.lookup("jms/BookStoreQueue");
+			ConnectionFactory cf = (ConnectionFactory)ctx.lookup("jms/RemoteConnectionFactory");
+			connection = cf.createConnection();
+			
+			Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
+			MessageProducer messageProducer = session.createProducer(queue);
+			
+			TextMessage message = session.createTextMessage("Hello World. The time is now " + new Date());
+			
+			messageProducer.send(message);
+			
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				connection.close();
+			} catch (JMSException e) {
+				
+			}
+		}
+
+	}
+
+}
